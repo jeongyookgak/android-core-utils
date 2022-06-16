4fb7a40e2bea04b350be4bf0d479904bc230d0f7
jhim <gurdlwl@gmail.com>
2022-06-16 12:26:49 +0900

    version up

--- utils/src/main/java/com/jeongyookgak/android/core/utils/extension/FragmentExt.kt
+++ utils/src/main/java/com/jeongyookgak/android/core/utils/extension/FragmentExt.kt
@@ -3,14 +3,6 @@ package com.jeongyookgak.android.core.utils.extension
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class FragmentExt {
-
-    fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
-        Toast.makeText(requireContext(), message, duration).show()
-    }
-
-    companion object {
-        const val TOAST_SHORT = Toast.LENGTH_SHORT
-        const val TOAST_LONG = Toast.LENGTH_LONG
-    }
+fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), message, duration).show()
 }
