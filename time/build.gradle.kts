7258d19a719e6edd9d5d3e919480c669c4500f8b
jhim <gurdlwl@gmail.com>
2022-06-21 16:19:38 +0900

    version up

--- time/build.gradle.kts
+++ time/build.gradle.kts
@@ -56,6 +56,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    implementation("com.jakewharton.threetenabp:threetenabp:1.3.0")
+    implementation("com.jakewharton.threetenabp:threetenabp:1.4.0")
 
 }
