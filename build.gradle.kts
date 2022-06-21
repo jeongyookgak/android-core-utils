4408167b01efdf60947b09d4aeb6f67496f3bf7c
jhim <gurdlwl@gmail.com>
2022-06-21 17:24:53 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ buildscript {
     }
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
