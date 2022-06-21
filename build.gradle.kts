6bedd3314ca8cfc1ef5e0658123db940f5b30038
jhim <gurdlwl@gmail.com>
2022-06-21 10:51:53 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,11 @@ buildscript {
     }
 
     dependencies {
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.google.gms:google-services:4.3.4")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
-}
-
-plugins {
-    id("com.android.application") version "7.2.0" apply false
-    id("com.android.library") version "7.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
 }
 
 task ("clean", Delete::class) {
