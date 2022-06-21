4a036727c0c52bc9ad9c4bf9ee4e2162280aa3b4
jhim <gurdlwl@gmail.com>
2022-06-21 14:02:32 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        classpath("com.google.gms:google-services:4.3.4")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
 }
