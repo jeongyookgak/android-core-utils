a3ef06a5620c641124db14bf35e4cbd7f6f7f0d1
jhim <gurdlwl@gmail.com>
2022-06-22 16:07:42 +0900

    change naming

--- utils/src/main/java/com/jeongyookgak/android/core/utils/util/analytics/AnalyticsHelperr.kt
+++ utils/src/main/java/com/jeongyookgak/android/core/utils/util/analytics/AnalyticsHelperr.kt
@@ -2,9 +2,9 @@ package com.jeongyookgak.android.core.utils.util.analytics
 
 import android.os.Bundle
 
-interface AnalyticsHelperr {
+interface AnalyticsHelper {
 
-    //    fun logEvent(event: Event)
+    // fun logEvent(event: Event)
     fun logEvent(eventName: String)
     fun logEvent(eventName: String, params: Map<String, Any?>)
     fun logEvent(eventName: String, params: Bundle)
--- utils/src/main/java/com/jeongyookgak/android/core/utils/util/analytics/FirebaseAnalyticsHelperr.kt
+++ utils/src/main/java/com/jeongyookgak/android/core/utils/util/analytics/FirebaseAnalyticsHelperr.kt
@@ -3,26 +3,17 @@ package com.jeongyookgak.android.core.utils.util.analytics
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import com.jeongyookgak.android.core.utils.extension.showToast
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 @SuppressLint("MissingPermission")
-class FirebaseAnalyticsHelperr @Inject constructor(
+class FirebaseAnalyticsHelper @Inject constructor(
     @ApplicationContext private val context: Context
-) : AnalyticsHelperr {
+) : AnalyticsHelper {
 
-    override fun logEvent(eventName: String) {
-        context.showToast("테스트!!")
-        Log.e("jhim", eventName)
-    }
+    override fun logEvent(eventName: String) { }
 
-    override fun logEvent(eventName: String, params: Map<String, Any?>) {
-        Log.e("jhim", eventName)
-    }
+    override fun logEvent(eventName: String, params: Map<String, Any?>) { }
 
-    override fun logEvent(eventName: String, params: Bundle) {
-        Log.e("jhim", eventName)
-    }
+    override fun logEvent(eventName: String, params: Bundle) { }
 }
